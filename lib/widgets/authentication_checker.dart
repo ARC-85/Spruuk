6b28587c0ff002dd15801af8659bb97e82e2d25e
Andrew Cameron <andrewrosscameron@gmail.com>
2023-01-18 01:42:42 +0000

    Template authentication progress

--- lib/widgets/authentication_checker.dart
+++ lib/widgets/authentication_checker.dart
@@ -2,6 +2,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:spruuk/providers/authentication_provider.dart';
+import 'package:spruuk/screens/home_screen.dart';
+import 'package:spruuk/screens/error_screen.dart';
+import 'package:spruuk/screens/authentication_screen.dart';
+import 'package:spruuk/screens/loading_screen.dart';
 
 class AuthenticationChecker extends ConsumerWidget {
   const AuthenticationChecker ({Key? key}) : super(key: key);
@@ -12,7 +16,7 @@ class AuthenticationChecker extends ConsumerWidget {
     return authState.when(
       data: (data) {
         if (data != null) return const HomePage();
-        return const LoginPage();
+        return const AuthenticationScreen();
       },
       loading: () => const LoadingScreen(),
       error: (e, trace) => ErrorScreen(e, trace)
