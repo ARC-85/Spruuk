34bfbf4d8dac3056ee28df57a8626b96af4f5167
Andrew Cameron <andrewrosscameron@gmail.com>
2023-01-18 01:45:49 +0000

    Template authentication progress

---
+++ lib/screens/authentication_screen.dart
@@ -0,0 +1,332 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:spruuk/providers/authentication_provider.dart';
+
+enum AuthStatus {
+  login,
+  signUp
+}
+
+class AuthenticationScreen extends StatefulWidget {
+  static const routename = '/AuthenticationPage';
+  const AuthenticationScreen({Key? key}) : super(key: key);
+
+  @override
+  _AuthenticationScreenState createState() => _AuthenticationScreenState();
+
+}
+
+class _AuthenticationScreenState extends State<AuthenticationScreen> {
+  //GlobalKey required to validate the form
+  final GlobalKey<FormState> _formKey = GlobalKey();
+
+  AuthStatus _authStatus = AuthStatus.login;
+
+  // TextEditingControllers for data inputs
+  final TextEditingController _email = TextEditingController(text: '');
+  final TextEditingController _password = TextEditingController(text: '');
+
+  // Bool variables for animation while loading
+  bool _isLoading = false;
+  bool _isLoadingGoogle = false;
+
+  // Variable for obscuring password
+  bool _obscureText = true;
+
+  // Method for setting the state of loading
+  void loading() {
+    setState(() {
+      _isLoading = !_isLoading;
+    });
+  }
+
+  // Method for setting the state of loading during Google sign in
+  void loadingGoogle() {
+    setState(() {
+      _isLoadingGoogle = !_isLoadingGoogle;
+    });
+  }
+
+  // Method for switching between login and sign up mode on authentication screen
+  void _switchType() {
+    if (_authStatus == AuthStatus.signUp) {
+      setState(() {
+        _authStatus = AuthStatus.login;
+      });
+    } else {
+      setState(() {
+        _authStatus = AuthStatus.signUp;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+        body: SafeArea(
+            child: Consumer(builder: (context, ref, _) {
+              final _auth = ref.watch(authenticationProvider);
+
+              Future<void> _onPressedFunction() async {
+                if (!_formKey.currentState!.validate()) {
+                  return;
+                }
+                if (_authStatus == AuthStatus.login) {
+                  loading();
+                  await _auth
+                      .signUpWithEmailAndPassword(
+                      _email.text,
+                      _password.text,
+                      context
+                  )
+                      .whenComplete(
+                          () =>
+                          _auth.authStateChange.listen((event) async {
+                            if (event == null) {
+                              loading();
+                              return;
+                            }
+                          })
+                  );
+                }
+              }
+
+              Future<void> _loginWithGoogle() async {
+                loadingGoogle();
+                await _auth
+                    .loginWithGoogle(context)
+                    .whenComplete(
+                        () =>
+                        _auth.authStateChange.listen((event) async {
+                          if (event == null) {
+                            loadingGoogle();
+                            return;
+                          }
+                        })
+                );
+              }
+
+              return Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    Expanded(
+                        flex: 3,
+                        child: Container(
+                            margin: const EdgeInsets.only(top: 48),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Center(child: FlutterLogo(size: 81)),
+                                const Spacer(flex: 1),
+                                Container(
+                                    margin: const EdgeInsets.symmetric(
+                                        horizontal: 24, vertical: 16),
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 16, vertical: 4),
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(
+                                            25)),
+                                    child: TextFormField(
+                                      controller: _email,
+                                      autocorrect: true,
+                                      enableSuggestions: true,
+                                      keyboardType: TextInputType.emailAddress,
+                                      onSaved: (value) {},
+                                      decoration: InputDecoration(
+                                        hintText: 'Email Address',
+                                        hintStyle: const TextStyle(
+                                            color: Colors.black45),
+                                        icon: Icon(Icons.email_outlined,
+                                            color: Colors.blue.shade700,
+                                            size: 24),
+                                        alignLabelWithHint: true,
+                                        border: InputBorder.none,
+                                      ),
+                                      validator: (value) {
+                                        if (value!.isEmpty || !value.contains('@')) {
+                                          return 'Invalid email!';
+                                        }
+                                        return null;
+                                      },
+                                    )
+                                ),
+                                Container(
+                                  margin: const EdgeInsets.symmetric(
+                                    horizontal: 24, vertical: 8),
+                                  padding: const  EdgeInsets.symmetric(
+                                    horizontal: 16, vertical: 4),
+                                  decoration: BoxDecoration(
+                                  color: Colors.white,
+                                    borderRadius: BorderRadius.circular(25)
+                                  ),
+                                  child: TextFormField(
+                                    controller: _password,
+                                    obscureText: true,
+                                    validator: (value) {
+                                      if (value!.isEmpty || value.length <8) {
+                                        return 'Password is too short!';
+                                      }
+                                      return null;
+                                    },
+                                    decoration: InputDecoration(
+                                      hintText: 'Password',
+                                      hintStyle: const TextStyle(color: Colors.black45),
+                                      icon: Icon(CupertinoIcons.lock_circle,
+                                      color: Colors.blue.shade700, size: 24),
+                                      alignLabelWithHint: true,
+                                      border: InputBorder.none,
+                                    ),
+                                  ),
+                                ),
+                                if (_authStatus == AuthStatus.signUp)
+                                  AnimatedContainer(
+                                      duration: const Duration(milliseconds: 600),
+                                      margin: const EdgeInsets.symmetric(
+                                      horizontal: 24, vertical: 8),
+                                      padding: const EdgeInsets.symmetric(
+                                      horizontal: 16, vertical: 4),
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(25)),
+                                      child: TextFormField(
+                                        obscureText: true,
+                                        decoration: InputDecoration(
+                                          hintText: 'Confirm password',
+                                          hintStyle: const TextStyle(color: Colors.black45),
+                                          icon: Icon(CupertinoIcons.lock_circle,
+                                              color: Colors.blue.shade700, size: 24),
+                                          alignLabelWithHint: true,
+                                          border: InputBorder.none,
+                                        ),
+                                        validator: _authStatus == AuthStatus.signUp
+                                        ? (value) {
+                                          if (value != _password.text) {
+                                            return 'Passwords do not match!';
+                                          }
+                                          return null;
+                                        }
+                                        : null,
+                                      ),
+                                  ),
+                                const Spacer()
+                              ],
+                            ),
+                        ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        width: double.infinity,
+                        decoration: const BoxDecoration(color: Colors.white),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.only(top: 32.0),
+                              margin: const EdgeInsets.symmetric(horizontal: 16),
+                              width: double.infinity,
+                              child: _isLoading
+                                ? const Center(
+                                child: CircularProgressIndicator()
+                              ) : MaterialButton(
+                                  onPressed: _onPressedFunction,
+                                textColor: Colors.blue.shade700,
+                                textTheme: ButtonTextTheme.primary,
+                                minWidth: 100,
+                                padding: const EdgeInsets.all(18,
+                                ),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(25),
+                                  side: BorderSide(
+                                    color: Colors.blue.shade700
+                                  ),
+                                ),
+                                child: Text(
+                                  _authStatus == AuthStatus.login
+                                      ? 'Login'
+                                      : 'Sign up',
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.w600),
+                                  ),
+                              ),
+
+                            ),
+                            Container(
+                              padding: const EdgeInsets.only(top: 32.0),
+                              margin: const EdgeInsets.symmetric(
+                                horizontal: 16
+                              ),
+                              width: double.infinity,
+                              child: _isLoadingGoogle
+                                ? const Center(
+                                child: CircularProgressIndicator())
+                                  : MaterialButton(
+                                onPressed: _loginWithGoogle,
+                                textColor: Colors.blue.shade700,
+                                textTheme: ButtonTextTheme.primary,
+                                minWidth: 100,
+                                padding: const EdgeInsets.all(18),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(25),
+                                  side: BorderSide(
+                                    color: Colors.blue.shade700),
+                                  ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: const [
+                                    FaIcon(FontAwesomeIcons.google),
+                                    Text(
+                                      'Login with Google',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w600),
+                                      ),
+                                  ],
+                                ),
+                                ),
+                              ),
+                            const Spacer(),
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 24.0),
+                              child: RichText(
+                                text: TextSpan(
+                                  text: _authStatus == AuthStatus.login
+                                      ? 'Don\'t have an account?'
+                                      : 'Already have an account?',
+                                  style: const TextStyle(color: Colors.black),
+                                  children: [
+                                    TextSpan(
+                                      text: _authStatus == AuthStatus.login
+                                          ? 'Sign up now'
+                                          : 'Log in',
+                                      style: TextStyle(
+                                        color: Colors.blue.shade700),
+                                  recognizer: TapGestureRecognizer()
+                                    ..onTap = () {
+                                        _switchType();
+                                  }
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        )
+                      ),
+                    )
+                  ],
+                ),
+              );
+            }
+            ),
+        ),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,54 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:spruuk/providers/authentication_provider.dart';
+
+class HomePage extends ConsumerWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // first variable is to get the data of Authenticated User
+    final data = ref.watch(authenticationProvider);
+
+    //  Second variable to access the Logout Function
+    final _auth = ref.watch(authenticationProvider);
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(/*data.currentUser!.email ??*/ 'You are logged In'),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(/*data.currentUser!.displayName ??*/
+                  ' Great you have Completed this step'),
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 48.0),
+              margin: const EdgeInsets.symmetric(horizontal: 16),
+              width: double.infinity,
+              child: MaterialButton(
+                onPressed: () => _auth.signOut(),
+                textColor: Colors.blue.shade700,
+                textTheme: ButtonTextTheme.primary,
+                minWidth: 100,
+                padding: const EdgeInsets.all(18),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25),
+                  side: BorderSide(color: Colors.blue.shade700),
+                ),
+                child: const Text(
+                  'Log Out',
+                  style: TextStyle(fontWeight: FontWeight.w600),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
